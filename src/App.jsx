7ba91793ae91274670d070b4646799c8795457a3
Andrew <andrewlin1368@gmail.com>
2024-01-03 15:49:01 -0500

    added hot reload fix and render puppy data

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,38 @@ import "./App.css";
 import React from "react";
 import { puppyList } from "./data";
 import { useState } from "react";
+import "./index.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  // console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  // console.log(featPupId);
+  const featurePup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featurePup);
   return (
     <>
       <div>
+        {featPupId && (
+          <div>
+            <h2>{featurePup.name}</h2>
+            <ul>
+              <li>Age: {featurePup.age}</li>
+              <li>Email: {featurePup.email}</li>
+            </ul>
+          </div>
+        )}
+
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })}
       </div>
     </>
