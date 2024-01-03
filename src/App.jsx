aa1474bad7e4c801fee79f4872bb440168104cdd
Andrew <andrewlin1368@gmail.com>
2024-01-03 14:58:08 -0500

    added sample data and render on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import React from "react";
+import { puppyList } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  // console.log(puppies);
   return (
     <>
       <div>
-        <h1>testing</h1>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
       </div>
     </>
   );
