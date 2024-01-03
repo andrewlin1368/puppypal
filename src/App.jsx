8a527cc1960f0155b37c6aaee05e864191433655
Andrew <andrewlin1368@gmail.com>
2024-01-03 13:52:36 -0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>testing</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
